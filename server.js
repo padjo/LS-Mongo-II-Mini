1ba7304f4fdc0283195a3c69bfb13bc474ed516c
pat collins <padjo@pats-MacBook-Pro-2.local>
2017-08-22 23:08:14 +0100

    working solution

--- server.js
+++ server.js
@@ -14,6 +14,76 @@ const STATUS_USER_ERROR = 422;
 app.use(bodyParser.json());
 
 // Your API will be built out here.
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  Person.findById(id, (err, user) =>{
+    if(err) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json({error:err});
+    } else {
+      res.json(user.friends);
+    }
+  });
+});
+
+app.get('/users', (req, res) => {
+  Person.find({}, (err, people) =>{
+    if(err) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json({error:err});
+    } else {
+      res.json(people);
+    }
+  });
+});
+
+app.get('/users/:direction', (req, res) => {
+  const { direction } = req.params;
+  let order = direction;
+
+  // console.log("Direction to sort by: ", direction);
+  if (direction != 'asc' && direction != 'desc') {
+    order = 'asc';
+    console.log("in loop" + order);
+
+  }
+  console.log(order);
+  Person.find({})
+  .sort({ firstName: order })
+  .exec((err, users) => {
+    if (err) {
+      res.status(STATUS_USER_ERROR);
+      res.json({ error: err });
+    } else {
+      res.json(users);
+    }
+  });
+});
+
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName } = req.body;
+  const updates = { firstName, lastName };
+  if (!firstName) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Please modify the FIRST NAME.' });
+    return;
+  }
+  if (!lastName) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Please modify the LAST NAME.' });
+    return;
+  }
+  Person.updateOne({ _id: id }, updates, (err) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json(`There is no: ${err.value}`);
+    } else {
+      res.json(updates);
+    }
+  });
+});
+
 
 
 mongoose.Promise = global.Promise;
